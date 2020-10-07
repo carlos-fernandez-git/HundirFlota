c1c1de8d74280bca6ad3b1bba24cfcd998635dc6
1490952 <adria.orozco@e-campus.uab.cat>
2020-10-07 16:11:35 +0200

    Prueba 2

--- src/test/java/HundirFlota/LibraryTest.java
+++ src/test/java/HundirFlota/LibraryTest.java
@@ -11,4 +11,8 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    @Test public void testSomeLibraryMethod2() {
+        Library classUnderTest = new Library();
+        assertFalse("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
 }
