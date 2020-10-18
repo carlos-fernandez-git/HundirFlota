82473bf35ebf596837367eeae4761f5b91f98b69
Arnau Cruz Gargallo <arnau.cruz@e-campus.uab.cat>
2020-10-18 22:49:41 +0200

    Basic methods and tests from Player implementation.

--- src/main/java/HundirFlota/Player.java
+++ src/main/java/HundirFlota/Player.java
@@ -3,15 +3,25 @@ package HundirFlota;
 public class Player {
 	private String Nombre;
 	private int turno;
-	public Player() {}
+	
+	public Player() {
+		
+	}
+	public Player(String nom) {
+		this.Nombre=nom;
+	}
 	public void setNombre(String nombre) {
-		
+		this.Nombre=nombre;
 	}
-	public String getNombre() {return "Hola";}
+	public String getNombre() {
+		return this.Nombre;
+	}
 	public void setTurno(int turno) {
-		
+		this.turno=turno;
 	}
-	public int getTurno() {return 0;}
+	public int getTurno() {
+		return this.turno;
+	}
 	
 
 }
--- src/test/java/HundirFlota/PlayerTest.java
+++ src/test/java/HundirFlota/PlayerTest.java
@@ -7,28 +7,31 @@ import org.junit.Test;
 public class PlayerTest {
 
 	@Test
-	public void test() {
-		Player b=new Player();
-		String res_0 = "Hola"; 
-		assertEquals(res_0,b.getNombre()); 
-	}
-	@Test
 	public void testconstructor() {
 		Player b=new Player();
 	}
 	@Test
+	public void testgetNombre() {
+		Player b=new Player("Adri");
+		String res_0 ="Adri";
+		assertEquals(res_0,b.getNombre());
+	}
+	@Test
 	public void testsetNombre() {
 		Player b=new Player();
+		b.setNombre("Arnau");
+		String res_0 = "Arnau";
+		assertEquals(res_0,b.getNombre());
 	}
 
 	@Test
-	public void testsetTurno() {
+	public void testsetygetTurno() {
 		Player b=new Player();
+		b.setTurno(2);
+		int res_0 = 2;
+		assertEquals(res_0,b.getTurno());
 	}
-	@Test
-	public void testgetTurno() {
-		Player b=new Player();
-	}
+
 
 
 }
