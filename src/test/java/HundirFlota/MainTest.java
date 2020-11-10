3b9aa3c3a43874d85de061e8326fa96c76ae6659
1490952 <adria.orozco@e-campus.uab.cat>
2020-11-11 00:13:55 +0100

    Removed empty test

--- src/test/java/HundirFlota/MainTest.java
+++ src/test/java/HundirFlota/MainTest.java
@@ -6,11 +6,5 @@ import org.junit.Test;
 
 public class MainTest {
 
-	@Test
-	public void test() {
-		Main b=new Main();
-		String res_0 = "Hola"; 
-		assertEquals(res_0,b.getNombre()); 
-	}
-
+	
 }
